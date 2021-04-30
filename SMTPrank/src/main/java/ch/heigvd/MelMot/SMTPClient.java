f2c345888ca304871bfb6462e3107944cc6e0677
alex-mottier <43849134+alex-mottier@users.noreply.github.com>
2021-04-30 19:42:59 +0200

    protocol ok

--- SMTPrank/src/main/java/ch/heigvd/MelMot/SMTPClient.java
+++ SMTPrank/src/main/java/ch/heigvd/MelMot/SMTPClient.java
@@ -14,7 +14,7 @@ public class SMTPClient {
     private PrintWriter out;
     private BufferedReader in;
     private String commonSeparator = " ";
-    final private String EOL = " \r\n";
+    final private String EOL = "\r\n";
 
     public SMTPClient(String url, int port) throws IOException {
         this.socket = new Socket(url,port);
@@ -28,14 +28,9 @@ public class SMTPClient {
         this.out.flush();
         String response = "";
         while((response = this.in.readLine()) != null){
-//            String[] message = response.split(commonSeparator);
-            System.out.println("Recieved: " + response);
-            switch(response){
-                case "250 Ok":
-                    System.out.print("Connection opened...");
-
-            }
-
+            System.out.println("Received: " + response);
+            if(response.contains("250 Ok"))
+                break;
         }
     }
 
@@ -63,15 +58,16 @@ public class SMTPClient {
         System.out.println("Received : " + response);
 
 
-        msg =  "from: <" + mail.getFrom() + ">" + EOL;
-        msg += "to: " + serialiseTos(mail.getTo()) + EOL;
-        msg += "date: " + new Date() + EOL;
-        msg += "subject: " + mail.getSubject() + EOL;
-        msg += "body: " + mail.getBody() + EOL;
-        msg += "." + EOL;
+        StringBuilder message = new StringBuilder();
+        message.append("from: <").append(mail.getFrom()).append(">").append(EOL);
+        message.append("to: ").append(serialiseTos(mail.getTo())).append(EOL);
+        message.append("date: ").append(new Date()).append(EOL);
+        message.append("subject: ").append(mail.getSubject()).append(EOL);
+        message.append("body: ").append(EOL).append(mail.getBody()).append(EOL);
+        message.append(".").append(EOL);
 
-        System.out.print("Send : " + msg);
-        this.out.print(msg);
+        System.out.print("Send : " + message);
+        this.out.print(message);
         this.out.flush();
         response = this.in.readLine();
         System.out.println("Received : " + response);
