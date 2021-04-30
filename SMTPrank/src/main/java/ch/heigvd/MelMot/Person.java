e156725ab630e623e71fa09b172f6cda26331f30
Jérémie Melly <melly.jeremie@gmail.com>
2021-04-30 17:11:15 +0200

    Class person

--- SMTPrank/src/main/java/ch/heigvd/MelMot/Person.java
+++ SMTPrank/src/main/java/ch/heigvd/MelMot/Person.java
@@ -1,4 +1,11 @@
 package ch.heigvd.MelMot;
 
 public class Person {
+    private String first_name;
+    private String last_name;
+    private final String address;
+
+    public Person(String address){
+        this.address = address;
+    }
 }
--- SMTPrank/src/main/java/ch/heigvd/MelMot/PrankGenerator.java
+++ SMTPrank/src/main/java/ch/heigvd/MelMot/PrankGenerator.java
@@ -1,4 +1,5 @@
 package ch.heigvd.MelMot;
 
 public class PrankGenerator {
+
 }
